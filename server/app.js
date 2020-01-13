1ae99b58755d5de49403c80cdc34284fe1ed0117
sanya-shch <shchipkov.sanya@ukr.net>
2020-01-13 23:59:48 +0200

    added mongoose models and updated resolve

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 3005;
+
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds263368.mlab.com:63368/graphql-tutorial', { useMongoClient: true });
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
 
 app.listen(PORT, err => {
     err ? console.log(err) : console.log('Server started!');
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,64 +2,67 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
 
-const movies = [
-    { id: '1', name: 'one', genre: 'oneone', directorId: '1' },
-    { id: '2', name: 'two', genre: 'twotwo', directorId: '2' },
-    { id: '3', name: 'three', genre: 'threethree', directorId: '3' },
-    { id: '4', name: 'four', genre: 'fourfour', directorId: '4' },
-    { id: '5', name: 'one2', genre: 'oneone2', directorId: '1' },
-    { id: '6', name: 'two2', genre: 'twotwo2', directorId: '1' },
-    { id: '7', name: 'three2', genre: 'threethree2', directorId: '3' },
-    { id: '8', name: 'four2', genre: 'fourfour2', directorId: '3' },
-];
-const directors = [
-    { id: '1', name: 'Quentin Tarantino', age: 55 },
-    { id: '2', name: 'Michael Radford', age: 72 },
-    { id: '3', name: 'James McTeigue', age: 51 },
-    { id: '4', name: 'Guy Ritchie', age: 50 },
-];
-// // Testing Query
-// query($id: ID) {
-//     movie(id: $id){
-//         id
-//         name
-//         genre
-//     }
-// }
-//
-// {
-//     "id": 3
-// }
-// // ******************
-// query($id: ID) {
-//     movie(id: $id){
-//         id
-//         name
-//         genre
-//         director {
-//             name
-//             age
-//         }
-//     }
-// }
-// // *******************
-// query {
-//     directors {
-//         id
-//         name
-//         age
-//     }
-// }
-// // *******************
-// query ($id: ID) {
-//     director (id: $id){
-//         id
-//         name
-//         movies {
-//             name
-//         }
-//     }
-// }
+const Movies = require('../models/movie');
+const Directors = require('../models/director');
+
+// const movies = [
+//     { id: '1', name: 'one', genre: 'oneone', directorId: '1' },
+//     { id: '2', name: 'two', genre: 'twotwo', directorId: '2' },
+//     { id: '3', name: 'three', genre: 'threethree', directorId: '3' },
+//     { id: '4', name: 'four', genre: 'fourfour', directorId: '4' },
+//     { id: '5', name: 'one2', genre: 'oneone2', directorId: '1' },
+//     { id: '6', name: 'two2', genre: 'twotwo2', directorId: '1' },
+//     { id: '7', name: 'three2', genre: 'threethree2', directorId: '3' },
+//     { id: '8', name: 'four2', genre: 'fourfour2', directorId: '3' },
+// ];
+// const directors = [
+//     { id: '1', name: 'Quentin Tarantino', age: 55 },
+//     { id: '2', name: 'Michael Radford', age: 72 },
+//     { id: '3', name: 'James McTeigue', age: 51 },
+//     { id: '4', name: 'Guy Ritchie', age: 50 },
+// ];
+// // // Testing Query
+// // query($id: ID) {
+// //     movie(id: $id){
+// //         id
+// //         name
+// //         genre
+// //     }
+// // }
+// //
+// // {
+// //     "id": 3
+// // }
+// // // ******************
+// // query($id: ID) {
+// //     movie(id: $id){
+// //         id
+// //         name
+// //         genre
+// //         director {
+// //             name
+// //             age
+// //         }
+// //     }
+// // }
+// // // *******************
+// // query {
+// //     directors {
+// //         id
+// //         name
+// //         age
+// //     }
+// // }
+// // // *******************
+// // query ($id: ID) {
+// //     director (id: $id){
+// //         id
+// //         name
+// //         movies {
+// //             name
+// //         }
+// //     }
+// // }
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
@@ -70,7 +73,8 @@ const MovieType = new GraphQLObjectType({
         director: {
             type: DirectorType,
             resolve(parent, args) {
-                return directors.find(director => director.id === parent.id);
+                // return directors.find(director => director.id === parent.id);
+                return Directors.findById(parent.directorId);
             }
         }
     }),
@@ -85,7 +89,8 @@ const DirectorType = new GraphQLObjectType({
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies.filter(movie => movie.directorId === parent.id);
+                // return movies.filter(movie => movie.directorId === parent.id);
+                return Movies.find({ directorId: parent.id });
             },
         },
     }),
@@ -98,26 +103,30 @@ const Query = new GraphQLObjectType({
             type: MovieType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return movies.find(movie => movie.id === args.id);
+                // return movies.find(movie => movie.id === args.id);
+                return Movies.findById(args.id);
             },
         },
         director: {
             type: DirectorType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return directors.find(director => director.id === args.id);
+                // return directors.find(director => director.id === args.id);
+                return Directors.findById(args.id);
             },
         },
         movies: {
             type: new GraphQLList(MovieType),
             resolve(parent, args) {
-                return movies;
+                // return movies;
+                return Movies.find({});
             }
         },
         directors: {
             type: new GraphQLList(DirectorType),
             resolve(parent, args) {
-                return directors;
+                // return directors;
+                return Directors.find({});
             }
         }
     }
