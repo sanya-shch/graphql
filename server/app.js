30d597f1c689fdfdf53f5569efc4d8a643cd7471
sanya-shch <shchipkov.sanya@ukr.net>
2020-01-15 23:55:29 +0200

    connecting frontend and backend

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
 
 mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds263368.mlab.com:63368/graphql-tutorial', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
